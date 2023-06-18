a84f410d2b4fafec4cec1ec8ba9d58046141bf0c
sibasish28 <126195097+sibasish28@users.noreply.github.com>
2023-06-19 02:46:42 +0530

    Initial commit

---
+++ seeds/index.js
@@ -0,0 +1,46 @@
+
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            author: '64804c42dbb85dfde7ca0067',
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            images: [
+                {
+                  url: 'https://res.cloudinary.com/dnqh5zrrr/image/upload/v1686342550/YelpCamp/g3zheix4ejeeihmm3mpe.jpg',
+                  filename: 'YelpCamp/g3zheix4ejeeihmm3mpe'
+                }
+              ],
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+            price
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
